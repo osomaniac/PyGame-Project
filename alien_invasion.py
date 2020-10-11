8a842329a050999c49cb797109ec7c97c1789bb9
OsoMania <suspensethebc@gmail.com>
2020-10-11 12:40:17 -0500

    Starting the Game Project

---
+++ alien_invasion.py
@@ -0,0 +1,27 @@
+import sys 
+import pygame 
+from settings import Settings
+
+class AlienInvasion:
+    def __init__(self):
+        ## initialize game
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        ## main loop for game
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            ## redraw screen during each pass through loop
+            self.screen.fill(self.settings.bg_color)
+            ## make recently drawn screen visible
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ## make a game instance and run game
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+## Class to store settings for Alien Invasion
+class Settings:
+    def __init__(self):
+        ## initalize settings
+        ''' Screen Settings '''
+        self.screen_width = 975
+        self.screen_height = 650
+        self.bg_color = (230,230,230)
